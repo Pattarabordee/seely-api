e795538845da4d4785b5ba692f16a3a2ba858672
Pattarabordee <pattarabordee.lpt@gmail.com>
2025-10-02 23:22:53 +0700

    feat(auth): add login flow

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+// import { CreateAuthDto } from './dto/create-auth.dto';
+import { LoginDto } from './dto/login.dto';
+import { TokensDto } from './dto/tokens.dto';
+// import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  login(loginDto: LoginDto): TokensDto {
+    const accessToken = "VALID-TOKEN"
+    return { accessToken };
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+  // create(createAuthDto: CreateAuthDto) {
+  //   return 'This action adds a new auth';
+  // }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+  // findAll() {
+  //   return `This action returns all auth`;
+  // }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} auth`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+  // update(id: number, updateAuthDto: UpdateAuthDto) {
+  //   return `This action updates a #${id} auth`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} auth`;
+  // }
 }
