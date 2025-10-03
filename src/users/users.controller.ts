77303c49e8236f5297afa8678163cc648cb9c0fd
Pattarabordee <pattarabordee.lpt@gmail.com>
2025-10-03 23:33:30 +0700

    feat(auth): implement jwt-strategy

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,43 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UseGuards } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { LoggedInDto } from '@app/auth/dto/logged-in.dto';
 import { PasswordRemoverInterceptor } from '@app/interceptors/password-remover.interceptor';
-import { JwtGuard } from '@app/auth/guards/jwt.guard';
-// import { UpdateUserDto } from './dto/update-user.dto';
+import { Body, Controller, Get, Post, Req, UseGuards, UseInterceptors } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @UseGuards(JwtGuard)
+  // GET /users/me + jwt-token
+  @UseGuards(AuthGuard('jwt'))
   @UseInterceptors(PasswordRemoverInterceptor)
-  @Get(':username')
-  findByUsername(@Param('username') username: string) {
-    return this.usersService.findByUsername(username);
+  @Get('me')
+  findByUsername(@Req() req: { user: LoggedInDto }) {
+    return this.usersService.findByUsername(req.user.username)
   }
 
-  // @Get()
-  // findAll() {
-  //   return this.usersService.findAll();
-  // }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.usersService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-  //   return this.usersService.update(+id, updateUserDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.usersService.remove(+id);
-  // }
 }
